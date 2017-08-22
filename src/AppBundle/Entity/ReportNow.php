1e3413153f208048d0de736d6e7a8f4a5a7436cb
radekkk000 <rkesiak@gmail.com>
2017-08-22 10:44:00 +0200

    Update ReportNow.php

--- src/AppBundle/Entity/ReportNow.php
+++ src/AppBundle/Entity/ReportNow.php
@@ -7,6 +7,7 @@ class ReportNow implements ReportInterface
     public function generateReport($benchmark, $date)
     {
         echo $date.' now report:'.'</br></br>';
+        //$benchmark as $benchmark ????????????????????????????????? mkoze $benchmarks daje $benchmark
         foreach($benchmark as $benchmark) {
             echo $benchmark->getTime().' '.$benchmark->getUrl().''.'</br>';
         }
